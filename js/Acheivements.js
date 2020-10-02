aa02dc816e9af846135ca1e4374cddee69e64322
Ahren McTaggart <awmctaggart@btinternet.com>
2020-10-02 15:45:54 +0100

    Finishing touches

--- js/Acheivements.js
+++ js/Acheivements.js
@@ -5,7 +5,7 @@ document.querySelectorAll(".Year").forEach((button) => {
     button.classList.toggle("Year--Active");
 
     if (button.classList.contains("Year--Active")) {
-      accordionData.style.maxHeight = accordionData.scrollHeight + 300 + "px";
+      accordionData.style.maxHeight = accordionData.scrollHeight + 1500 + "px";
     } else {
       accordionData.style.maxHeight = 0;
     }
@@ -19,7 +19,35 @@ document.querySelectorAll(".jobbutton").forEach((button) => {
     button.classList.toggle("Job--Active");
 
     if (button.classList.contains("Job--Active")) {
-      accordionData.style.maxHeight = accordionData.scrollHeight + 700 + "px";
+      accordionData.style.maxHeight = accordionData.scrollHeight + 1500 + "px";
+    } else {
+      accordionData.style.maxHeight = 0;
+    }
+  });
+});
+
+document.querySelectorAll(".certbutton").forEach((button) => {
+  button.addEventListener("click", () => {
+    const accordionData = button.nextElementSibling;
+
+    button.classList.toggle("Cert--Active");
+
+    if (button.classList.contains("Cert--Active")) {
+      accordionData.style.maxHeight = accordionData.scrollHeight + 1500 + "px";
+    } else {
+      accordionData.style.maxHeight = 0;
+    }
+  });
+});
+
+document.querySelectorAll(".spacebutton").forEach((button) => {
+  button.addEventListener("click", () => {
+    const accordionData = button.nextElementSibling;
+
+    button.classList.toggle("Space--Active");
+
+    if (button.classList.contains("Space--Active")) {
+      accordionData.style.maxHeight = accordionData.scrollHeight + 800 + "px";
     } else {
       accordionData.style.maxHeight = 0;
     }
